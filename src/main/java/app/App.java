3a61f8864e8e8b60d6dafaeac3c6e7cf7922413d
Gabriel Salazar <32026770+cubicles@users.noreply.github.com>
2021-10-23 13:07:00 -0500

    Base Test case setup

---
+++ src/main/java/app/App.java
@@ -0,0 +1,9 @@
+package app;
+
+import java.util.logging.Logger;
+
+public class App {
+    private static Logger logger = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) {logger.info("Se inicia la aplicación");}
+}
